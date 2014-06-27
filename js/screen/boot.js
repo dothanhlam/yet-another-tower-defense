3e295a1cd35b79189794732456b44c7714f15fdb
Lam Do <dothanhlam@gmail.com>
2014-06-28 00:48:55 +0700

    splash screen and loading bar. resolve #6

--- js/screen/boot.js
+++ js/screen/boot.js
@@ -1,17 +1,18 @@
 /**
  * 
  */
-define([], function() {
+define(['helper/resource'], function(R) {
 	Boot = function(game) {
 		this.game = game;
 	};
 
 	Boot.prototype = {
 		preload : function() {
+			R.game = this.game;
+			 R.loadImage(["splash.png"]);
 		},
 
-		create : function() {
-		
+		create : function() {		
 			this.game.state.start('Preloader');
 		}
 	};
--- js/screen/preloader.js
+++ js/screen/preloader.js
@@ -10,6 +10,13 @@ define(['phaser', 'helper/resource'], function(Phaser, R) {
 
 	Preloader.prototype = {
 		preload : function() {
+			this.game.add.image(0, 0, "splash");
+			
+			this.graphics = this.game.add.graphics(0, 0);
+			this.graphics.lineStyle(2, 0xFFFF00, 1);
+		    this.graphics.drawRect((this.game.width - 200) / 2, (this.game.height + 100) / 2, 200, 10);
+		    this.graphics.lineStyle(1, 0xFFCC00, 1);
+		    
 			R.game = this.game;
 			R.loadTiledMap(["default"]);
 		    R.loadImage(["car.png","bullet.png","tiles.png","menu.png"]);
@@ -19,11 +26,20 @@ define(['phaser', 'helper/resource'], function(Phaser, R) {
 		    R.loadSingleSpriteSheet("play", 47, 48, 1);
 		    R.loadSingleSpriteSheet("options", 47, 48, 1);
 		    R.loadSingleSpriteSheet("quit", 47, 48, 1);		    
+		    
+		    this.game.load.onFileComplete.add(this.loadHandler, this);
 		},
 
-		create : function() {
+		create : function() {			
 			this.game.state.start('Play');
-		}
+		},
+		
+		loadHandler: function(progress){ 
+            this.graphics.beginFill(0xFFCC00, 1);
+            this.graphics.drawRect((this.game.width - 196) / 2, (this.game.height + 100) / 2 + 2, 195 * progress / 100, 5);
+            this.graphics.endFill();
+        },
+
 	};
 
 	return Preloader;
